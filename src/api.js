e4e5c64e3ebaa2fddcd0b89dcb132f38bb7ebbe2
Shanmukh <karnamshanmukh@karnams-MacBook-Air.local>
2025-12-30 08:08:35 +0530

    Frontend ready for deployement

--- src/api.js
+++ src/api.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://eventhub-backend.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
+  if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
 });
 
